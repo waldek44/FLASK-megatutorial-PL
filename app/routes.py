2a0e820eb054d619569595b5612a7465b677d426
Waldek <reclikmail@gmail.com>
2020-04-28 21:12:54 +0200

    2 | TEMPLATES

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,24 @@
+from flask import render_template
 from app import app
 
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+
+    # dodaję tymczasowego użytkownika, którego zamierzam zaimplementować jako słownik (dictionary)
+    user = {'username': 'Waldemar'}
+
+    # dodaję tymczasowe posty
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        }
+    ]
+
+    return render_template('index.html', title='Strona domowa', user=user, posts=posts)
